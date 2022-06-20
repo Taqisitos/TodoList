d03aa25293819dd632fbf89ac1d805fa21ab52ae
Taqi Hossain <taqi.hossain@gmail.com>
2022-06-20 17:44:25 -0500

    writing tests in progress

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -2,7 +2,7 @@
 pragma solidity >=0.4.22 <0.9.0;
 
 contract TodoList {
-  uint taskCount = 0;
+  uint public taskCount = 0;
 
     struct Task {
         uint id;
--- test/todo_list.js
+++ test/todo_list.js
@@ -5,9 +5,20 @@ const TodoList = artifacts.require("TodoList");
  * Ethereum client
  * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
  */
-contract("TodoList", function (/* accounts */) {
-  it("should assert true", async function () {
-    await TodoList.deployed();
-    return assert.isTrue(true);
+contract("TodoList", function (accounts) {
+  let alice = accounts[0];
+  let contractInstance;
+  beforeEach(async () => {
+    contractInstance = await TodoList.new();
+  });
+  it("should update task count correctly", async function () {
+    await contractInstance.createTask("Task 1", {from: alice});
+    const result = await contractInstance.taskCount();
+    assert.equal(result.toNumber(), 1);
+  });
+  it("should toggle task complete correctly", async () => {
+    await contractInstance.markComplete(0, {from: alice}); // mark task 0 complete
+    const task = await contractInstance.tasks[0];
+    assert.equal(task.completed, true);
   });
 });
